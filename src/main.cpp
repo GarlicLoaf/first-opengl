ddbe010caf9a06ae8bf10b7506cad8d38a297943
GarlicLoaf <garic15@protonmail.com>
2025-06-14 16:54:50 +0200

    First colored triangle!

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,19 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+extern const char* vertex_shader_code;
+extern const char* fragment_shader_code;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-int main() {
+GLFWwindow* window_setup() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Firs Project", NULL, NULL);
@@ -24,40 +23,93 @@ int main() {
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << "\n";
         glfwTerminate();
-        return -1;
+        return NULL;
     }
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        return NULL;
     }
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    return window;
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void send_data() {
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
     // clang-format off
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+    GLfloat verts[]{0.0f, 1.0f,
+                    1.0, 0.0, 0.0,
+                    1.0f, -1.0f,
+                    0.0, 1.0, 0.0,
+                    -1.0f, -1.0f, 
+                    0.0, 0.0, 1.0
     };
     // clang-format on
 
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GLuint myBufferID;
+    glGenBuffers(1, &myBufferID);
+    glBindBuffer(GL_ARRAY_BUFFER, myBufferID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                          (char*)(2 * sizeof(float)));
+
+    GLushort indices[] = {0, 1, 2};
+    GLuint indexBufferID;
+    glGenBuffers(1, &indexBufferID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+}
+
+void install_shaders() {
+    GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const char* adapter[1];
+    adapter[0] = vertex_shader_code;
+    glShaderSource(vertex_shader_id, 1, adapter, 0);
+    adapter[0] = fragment_shader_code;
+    glShaderSource(fragment_shader_id, 1, adapter, 0);
+
+    glCompileShader(vertex_shader_id);
+    glCompileShader(fragment_shader_id);
+
+    GLuint program_id = glCreateProgram();
+    glAttachShader(program_id, vertex_shader_id);
+    glAttachShader(program_id, fragment_shader_id);
+    glLinkProgram(program_id);
+
+    glUseProgram(program_id);
+}
+
+int main() {
+    GLFWwindow* window = window_setup();
+    send_data();
+    install_shaders();
 
     while (!glfwWindowShouldClose(window)) {
         // input
         processInput(window);
 
         // rendering
-        glad_glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // buffer check
+        // glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
